c9c3972a1acc6dd4fdf13faf684303ef73a95b4c
CarolinaGripsborn <carolina.gripsborn@gmail.com>
2019-02-14 12:22:29 +0000

    skeleton for customer order page

--- app.js
+++ app.js
@@ -35,6 +35,11 @@ app.get('/driver', function (req, res) {
 app.get('/dispatcher', function (req, res) {
   res.sendFile(path.join(__dirname, 'views/dispatcher.html'));
 });
+// Serve customer_order.html as /order
+app.get('/order', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views/customer_order.html'));
+});
+
 
 // Store data in an object to keep the global namespace clean and 
 // prepare for multiple instances of data if necessary
